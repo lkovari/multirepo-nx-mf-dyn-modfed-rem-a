513a9a80ebe892a1745ee39e44107e4eded701c1
László Kővári <laszlo.kovary@gmail.com>
2026-03-22 21:43:39 +0100

    add last update

--- nxdmfmf-remote-a-workspace/apps/nxdmfmf-remote-a/src/app/remote-entry/remote-a-main.ts
+++ nxdmfmf-remote-a-workspace/apps/nxdmfmf-remote-a/src/app/remote-entry/remote-a-main.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+const LAST_UPDATE = '2025-03-22 15:43:00';
+
 @Component({
   selector: 'app-remote-a-main',
   imports: [],
@@ -7,4 +9,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './remote-a-main.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RemoteAMain {}
+export class RemoteAMain {
+  protected readonly lastUpdate = LAST_UPDATE;
+}
