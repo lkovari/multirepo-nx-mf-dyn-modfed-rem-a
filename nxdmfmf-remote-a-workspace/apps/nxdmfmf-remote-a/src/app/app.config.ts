b1643f512543dd85b16d2ce6838dfc7bbeb5358f
László Kővári <laszlo.kovary@gmail.com>
2026-04-29 02:26:54 +0200

    Add primeng

--- nxdmfmf-remote-a-workspace/apps/nxdmfmf-remote-a/src/app/app.config.ts
+++ nxdmfmf-remote-a-workspace/apps/nxdmfmf-remote-a/src/app/app.config.ts
@@ -2,9 +2,21 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    provideRouter(appRoutes),
+  ],
 };
